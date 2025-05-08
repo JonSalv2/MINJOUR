8121476b4220822343baa4453d3569adb038bc5a
jonathan cozier <jonathancozier@jonathans-MacBook-Pro.local>
2025-05-08 14:13:48 -0700

    added read file entires function

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import os
 FILE_NAME = "entries.json"
 
 
-def write_to_file(filename, new_data):
+def write_to_file(file_name, new_data):
 
     # checks to see if the file exists
-    if os.path.exists(filename):
-        with open(filename, "r") as file:
+    if os.path.exists(file_name):
+        with open(file_name, "r") as file:
             existing_entries = json.load(file)
     else:
         existing_entries = []
@@ -21,7 +21,7 @@ def write_to_file(filename, new_data):
     existing_entries.extend(new_data)
 
     # write to file
-    with open(filename, mode='w') as file:
+    with open(file_name, mode='w') as file:
         json.dump(existing_entries, file, indent=4)
 
 
@@ -39,13 +39,23 @@ def add_entry():
     return entry_data
 
 
+def show_entries(file_name):
+
+    if os.path.exists(file_name):
+        with open(file_name, "r") as file:
+            existing_entries = json.load(file)
+
+            for entry in existing_entries:
+                print(f"{entry['time_stamp']}: {entry['entry']}")
+
+
 def main():
 
     global FILE_NAME
 
     print("\n~*| MINJOUR |*~\n")
     print("A simplistic journaling app to get your ideas down quickly, in 500 characters.")
-    user_input= input("press enter to continue\n")
+    user_input= input("press (enter) for a new entry\npress (r) to read your entries\npress (q) to quit\n")
 
     if user_input == "":
         while True:
@@ -62,6 +72,14 @@ def main():
             else:
                 return
 
+    elif user_input == "r":
+        print("~~~~~~((((Your entries))))~~~~~~\n")
+        show_entries(FILE_NAME)
+        return
+    
+    elif user_input == "q":
+        print("Goodbye!")
+        return
 
 if __name__ == '__main__':
     main()
